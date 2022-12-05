75987d4672b1038fd32ff2e25d6b9104b7fa7b36
Dmitrii Elkin <yolkindm@mail.ru>
2022-12-05 17:06:26 +0300

    HomeWork 3_2

---
+++ 3_HomeWork/3_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void Squares(int n)
+{
+    double[] nums = new double[n];
+    int index = 0;
+
+    Console.Write($"Таблица кубов чисел от 1 до введенного числа -> ");
+
+    while (index < n)
+    {
+        nums[index] = Math.Pow((index + 1), 3);
+        Console.Write($"{nums[index]}, ");
+        index += 1;
+    }    
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите число");
+Squares(int.Parse(Console.ReadLine()));
