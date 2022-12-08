f997ce90030f2d553cff514c2ad34e2dfaee9dfb
Dmitrii Elkin <yolkindm@mail.ru>
2022-12-08 09:31:30 +0300

    HomeWork 4_2

---
+++ 4_HomeWork/4_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] Massive(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(1, 100);
+    return arr;
+}
+
+int[] arr_1 = Massive(int.Parse(Console.ReadLine()));
+Print(arr_1);
