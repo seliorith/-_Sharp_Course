fc760959afaa9a810105c1f0b84dbae84dc03e8b
Dmitrii Elkin <yolkindm@mail.ru>
2022-12-12 08:45:06 +0300

    HomeWork 5_1

---
+++ 5_HomeWork/5_1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 26
+// [-4, -6, 4, 6] -> 0
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] Mass(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(-100, 100);
+    return arr;
+}
+
+void SumOddNum(int[] arr)
+{
+    int OddNum = 0;
+
+    for (int i = 0; i < arr.Length; i += 2)
+    {
+        OddNum += arr[i];
+    }
+    Console.WriteLine($"Сумма нечетных чисел: {OddNum}");
+}
+
+int[] arr = Mass(int.Parse(Console.ReadLine()));
+Print(arr);
+SumOddNum(arr);
