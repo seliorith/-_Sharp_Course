d618b717bc3c491d619f2b4a8c90be63449ee595
Dmitrii Elkin <yolkindm@mail.ru>
2022-12-12 08:27:28 +0300

    HomeWork 5_0

---
+++ 5_HomeWork/5_0/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] Mass(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(100, 1000);
+    return arr;
+}
+
+void EvenNum(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count += 1;
+    }
+    Console.WriteLine($"Количество четных чисел: {count}");
+}
+
+int[] arr = Mass(int.Parse(Console.ReadLine()));
+Print(arr);
+EvenNum(arr);
