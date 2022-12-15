73eaf7efa13478f685b6888a7d93433763112e34
Dmitrii Elkin <yolkindm@mail.ru>
2022-12-15 08:24:35 +0300

    HomeWork 6

---
+++ 6_HomeWork/6_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void Intersection(double b1, double k1, double b2, double k2)
+{
+    double k_sub = k1 - k2;
+    if (k_sub != 0)
+    {
+        double x = (b2 - b1) / k_sub;
+        double y = k1 * x + b1;
+        Console.WriteLine($"Точка пересечения: ({x}; {y})");
+    }
+}
+Console.WriteLine("Введите точки для построения двух прямых");
+Intersection(int.Parse(Console.ReadLine()),
+             int.Parse(Console.ReadLine()),
+             int.Parse(Console.ReadLine()),
+             int.Parse(Console.ReadLine()));
