4f8bfaf46dfc3a3b3d82c58919a49f4b2927cb84
Dmitrii Elkin <yolkindm@mail.ru>
2022-12-18 19:23:50 +0300

    HomeWork 7_1

--- 7_HomeWork/7_0/Program.cs
+++ 7_HomeWork/7_0/Program.cs
@@ -1,8 +1,8 @@
-﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
+﻿// // Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// // m = 3, n = 4.
+// // 0,5 7 -2 -0,2
+// // 1 -3,3 8 -9,9
+// // 8 7,8 -7,1 9
 
 void Print(double[,] arr)
 {
@@ -25,14 +25,13 @@ double[,] MassNums(int row, int column, int from, int to)
 
     for (int i = 0; i < row; i++)
         for (int j = 0; j < column; j++)
-            arr[i, j] = Math.Round(n_new.NextDouble() * -10, 2);
-
+            arr[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
     return arr;
 }
 
-Console.Write("Enter the number of rows: ");
+Console.Write("Введите количество строк: ");
 int row = int.Parse(Console.ReadLine());
-Console.Write("Enter the number of columns: ");
+Console.Write("Введите количество столбцов: ");
 int column = int.Parse(Console.ReadLine());
 
 double[,] arr_1 = MassNums(row, column,
