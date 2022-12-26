19871d0154998953b37d58d6eb0fec1f4cdfed80
Dmitrii Elkin <yolkindm@mail.ru>
2022-12-26 06:55:21 +0300

    HomeWork 9

---
+++ 9_HomeWork/9_0/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Задайте значение N. Напишите программу, которая выведет все четные натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "2, 4"
+// N = 8 -> "2, 4, 6, 8"
+
+int SetEvenNaturalNumbers(int num)
+{
+    if (num <= 1) return num;
+    if (num % 2 == 0)
+    {
+        Console.Write($"{num}, ");
+        return SetEvenNaturalNumbers(num - 2);
+    }
+    else return SetEvenNaturalNumbers(num - 1);
+}
+
+SetEvenNaturalNumbers(5);
+Console.WriteLine();
+SetEvenNaturalNumbers(8);
---
+++ 9_HomeWork/9_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNaturalElements(int M, int N)
+{
+    if (M > N) return 0;
+    return SumNaturalElements(M + 1, N) + M;
+}
+
+Console.WriteLine(SumNaturalElements(1, 15));
+Console.WriteLine(SumNaturalElements(4, 8));
